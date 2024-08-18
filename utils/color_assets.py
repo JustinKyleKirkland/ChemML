0371acf8e337e0f41cb01da803ab7b175ebde32b
JustinKKirkland <justin.kirkland.phd@gmail.com>
2024-08-18 02:00:36 -0400

    Updated colors

--- utils/color_assets.py
+++ utils/color_assets.py
@@ -1,7 +1,7 @@
 from PyQt5.QtGui import QColor
 
 # Define color constants
-NAN_COLOR = QColor(99, 7, 39)  # Dark red for errors
-NON_NUM_COLOR = QColor(0, 102, 204)  # Blue for information
+NON_NUM_COLOR = QColor(0, 100, 100)  # Cyan color for errors
+NAN_COLOR = QColor(100, 0, 100)  # Magenta color for information
 BUTTON_COLOR = QColor(0, 122, 255)  # Blue color for buttons
-ERROR_COLOR = QColor(255, 0, 0)  # Red color for errors
+ERROR_COLOR = QColor(220, 53, 69)  # Red color for errors
